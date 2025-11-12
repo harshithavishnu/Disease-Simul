292d01c0761c5b42611825c73b031522626d790b
harshithavishnu <mailtoharshithavishnu@gmail.com>
2025-11-11 22:16:03 -0800

    Start Button changes color when user clicks Changed bg of start screen

--- src/Game.java
+++ src/Game.java
@@ -45,12 +45,25 @@ public class Game extends PApplet {
 
     public void drawStartScreen() {
         background(30);
+
+        for (int i = 0; i < 100; i++) {
+            float px = random(width);
+            float py = random(height);
+            fill(255, 100);
+            noStroke();
+            ellipse(px, py, 5, 5);
+        }
+
         textAlign(CENTER);
         textSize(50);
+        fill(0);
+        text("Disease Simulation", width/2 + 5, height/2 - 95);
         fill(255);
         text("Disease Simulation", (float) width /2, (float) height /2 - 100);
 
+
         // draw start button
+
         fill(0, 150, 255);
         rect((float) width /2 - 100, (float) height /2, 200, 80, 20);
         fill(255);
@@ -74,10 +87,14 @@ public class Game extends PApplet {
     public void mousePressed() {
         if (state == 0) {
             // check if user clicked inside start button
-            if (mouseX > width/2 - 100 && mouseX < width/2 + 100 &&
-                    mouseY > height/2 && mouseY < height/2 + 80) {
+            if (mouseX > width / 2 - 100 && mouseX < width / 2 + 100 &&
+                    mouseY > height / 2 && mouseY < height / 2 + 80) {
                 state = 1; // start simulation
+                fill(0, 200, 255);
+            } else {
+                fill(0, 150, 255);
             }
+            rect(width / 2 - 100, height / 2, 200, 80, 20);
         }
     }
 
